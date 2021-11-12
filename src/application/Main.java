e66c675b2b988159f6182e6d705bf7b72a39b8cb
nonbunrah <rj.bamrah@gmail.com>
2021-11-11 17:34:16 -0800

    File name change and got pages to work

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,19 +1,20 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			GridPane root = (GridPane)FXMLLoader.load(getClass().getResource("RevoTea.fxml"));
+//			VBox root = (VBox)FXMLLoader.load(getClass().getResource("RevoTea.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/application/Login.fxml"));
 			
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,500,800);
 			
 			primaryStage.setScene(scene);
 			primaryStage.show();
--- src/application/RevoTeaController.java
+++ src/application/RevoTeaController.java
@@ -1,5 +1,62 @@
 package application;
-
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.stage.Window;
 public class RevoTeaController {
-	
+	@FXML
+    private TextField nameField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private Button submitButton;
+
+    @FXML
+    public void SignUp(ActionEvent event) throws Exception {
+    	Stage primaryStage = new Stage();
+		Parent root = FXMLLoader.load(getClass().getResource("/application/SignUp.fxml"));
+		Scene scene = new Scene(root,400,400);
+		scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setScene(scene);
+		primaryStage.show();
+        Window owner = submitButton.getScene().getWindow();
+        if(nameField.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, owner, "Form Error!", 
+                    "Please enter your name");
+            return;
+        }
+        if(emailField.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, owner, "Form Error!", 
+                    "Please enter your email id");
+            return;
+        }
+        if(passwordField.getText().isEmpty()) {
+            showAlert(Alert.AlertType.ERROR, owner, "Form Error!", 
+                    "Please enter a password");
+            return;
+        }
+
+        showAlert(Alert.AlertType.CONFIRMATION, owner, "Registration Successful!", 
+                "Welcome " + nameField.getText());
+    }
+    public static void showAlert(Alert.AlertType alertType, Window owner, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.initOwner(owner);
+        alert.show();
+    }
 }
